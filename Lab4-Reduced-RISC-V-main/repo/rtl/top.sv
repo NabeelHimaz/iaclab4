916a5ab1f495e30a6dfedb5efb78ffce74ab1e1b
Nabeel Himaz <danab1710@gmail.com>
2025-11-19 13:00:46 +0000

    topop

--- Lab4-Reduced-RISC-V-main/repo/rtl/top.sv
+++ Lab4-Reduced-RISC-V-main/repo/rtl/top.sv
@@ -62,14 +62,36 @@ module top #(
     logic [4:0] rs1;
     logic [4:0] rs2;
     logic [4:0] rd;
-    logic [DATA_WIDTH-1:0] ALUout;
 
-    assign 
+    always_comb begin
+        case(instr[6:0])
+            7'b0000011: begin
+                assign rd = instr[11:7];
+            end
+            7'b0100011: begin
+                assign rs2 = instr[24:20];
+            end
+            7'b1100011: begin
+                assign rs2 = instr[24:20];
+            end
+            default begin
+                assign rs1 = instr[19:15];
+                assign rs2 = 5'b0;
+                assign rd = b'b0;
+            end
+    end
 
     regfile regfile(
         .clk(clk),
         .AD1(rs1),
+        .AD2(rs2),
+        .AD3(rd),
+        .WE3(RegWrite),
+        .WD3(ALUout),
 
+        .RD1(ALUop1),
+        .RD2(regOp2),
+        .a0(a0)
     )
 
    
